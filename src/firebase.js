cb71e824bb8104a5a2f09b22b2b645aa1ece557e
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-02-27 01:22:46 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzrSMuq88mKoFSmLWHpPpegrTKyrCYFxU",
+  authDomain: "chat-d71e0.firebaseapp.com",
+  projectId: "chat-d71e0",
+  storageBucket: "chat-d71e0.appspot.com",
+  messagingSenderId: "612491305987",
+  appId: "1:612491305987:web:683d9dc8cfb7807a879a36"
+};
+// console.log(firebaseConfig.apiKey)
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore()
